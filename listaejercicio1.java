3a8418fc1d485923f1b9c52fc701f9b59c00dc22
paolos100 <142361882+paolos100@users.noreply.github.com>
2023-09-17 21:05:46 -0500

    Create listaejercicio1.java

---
+++ listaejercicio1.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int x = 144;
+        int y = 999;
+     
+        System.out.println("El valor de x es: " + x);
+        System.out.println("El valor de y es: " + y);
+        
+        int suma = x + y;
+        int resta = x - y;
+        int division = x / y;
+        int multiplicacion = x * y;
+        
+        System.out.println("La suma de x e y es: " + suma);
+        System.out.println("La resta de x e y es: " + resta);
+        System.out.println("La división de x entre y es: " + division);
+        System.out.println("La multiplicación de x e y es: " + multiplicacion);
+    }
+}
