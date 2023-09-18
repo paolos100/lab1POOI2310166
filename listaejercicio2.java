4b10cb8a6af81d7f8fc3629a3ca22847967aaa8d
paolos100 <142361882+paolos100@users.noreply.github.com>
2023-09-17 21:06:40 -0500

    Create listaejercicio2.java

---
+++ listaejercicio2.java
@@ -0,0 +1,13 @@
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Paolo";
+        String direccion = "Calle Castilla La Nueva 136";
+        String telefono = "993-623-729";
+        
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Dirección: " + direccion);
+        System.out.println("Teléfono: " + telefono);
+    }
+}
